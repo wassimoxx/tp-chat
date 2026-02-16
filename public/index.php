e165bcc6e0cece2f996cd18631328c04d0dfbd73
wassimoxx <dakhiawassim77@gmail.com>
2026-02-16 19:19:10 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,11 @@
 <?php
 
-// Show errors (debug only)
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Project root
 define('BASE_PATH', dirname(__DIR__));
 
-// Load router file
 require_once BASE_PATH . '/app/Core/Router.php';
 
-// IMPORT the class namespace (IMPORTANT)
-use App\Core\Router;
-
-// Run app
 $router = new Router();
-$router->dispatch();   // or run() if your router uses that
+$router->dispatch();   // or run() if dispatch not exist
