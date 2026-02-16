25960b6b25d4c35947d0481beb5016d0172f192e
wassimoxx <dakhiawassim77@gmail.com>
2026-02-16 19:32:53 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 // Project root
 define('BASE_PATH', dirname(__DIR__));
 
-// Define base URL (IMPORTANT FIX)
+// Define base URL
 define('BASE_URL', '/');
 
 // Load router
@@ -15,6 +15,15 @@ require_once BASE_PATH . '/app/Core/Router.php';
 
 // Import namespace
 use Core\Router;
+
+/* ======================================
+   REDIRECT HOMEPAGE (FIX 404)
+   Change /login to your real first page
+====================================== */
+if ($_SERVER['REQUEST_URI'] === '/') {
+    header('Location: /login');   // change if needed
+    exit;
+}
 
 // Run application
 $router = new Router();
